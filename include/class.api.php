7a894fb857a0ad6013455c4d732a639f36e28bb4
Marco <marco.salmi89@gmail.com>
2022-07-12 19:29:28 +0200

    Fix

--- include/class.api.php
+++ include/class.api.php
@@ -168,7 +168,7 @@ class ApiController {
 
     var $apikey;
 
-    function requireApiKey() {
+    /*function requireApiKey() {
         # Validate the API key -- required to be sent via the X-API-Key
         # header
 
@@ -184,6 +184,25 @@ class ApiController {
 
         if (!$this->apikey && isset($_SERVER['HTTP_X_API_KEY']) && isset($_SERVER['REMOTE_ADDR']))
             $this->apikey = API::lookupByKey($_SERVER['HTTP_X_API_KEY'], $_SERVER['REMOTE_ADDR']);
+
+        return $this->apikey;
+    }*/
+
+    function requireApiKey() {
+
+
+        if(!($key=$this->getApiKey()))
+            return $this->exerr(401, __('Valid API key required'));
+        elseif (!$key->isActive())
+            return $this->exerr(401, __('API key not found/active or source IP not authorized'));
+
+        return $key;
+    }
+
+    function getApiKey() {
+
+        if (!$this->apikey && isset($_SERVER['HTTP_X_API_KEY']))
+            $this->apikey = API::lookupByKey($_SERVER['HTTP_X_API_KEY']);
 
         return $this->apikey;
     }
