d2678263306342d437e426fa2b6488ce3dbae5ac
Marco <marco.salmi89@gmail.com>
2022-07-13 10:00:29 +0200

    Fix

--- include/staff/ticket-view_new.inc.php
+++ include/staff/ticket-view_new.inc.php
@@ -1,7 +1,7 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+/*error_reporting(E_ALL);
+ini_set("display_errors", 1);*/
 
 //Note that ticket obj is initiated in tickets.php.
 if(!defined('OSTSCPINC') || !$thisstaff || !is_object($ticket) || !$ticket->getId()) die('Invalid path');
