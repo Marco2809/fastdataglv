ae8a6e981bb9094d72519c755f0d8c1dd6ada19b
Marco <marco.salmi89@gmail.com>
2022-08-02 10:20:32 +0200

    Fix

--- testo.php
+++ testo.php
@@ -1,6 +1,15 @@
 <?php
 
-phpinfo();
+//phpinfo();
+
+if ( function_exists( 'mail' ) )
+{
+    echo 'mail() is available';
+}
+else
+{
+    echo 'mail() has been disabled';
+}
 
 $servername = "localhost";
 $username = "admin";
