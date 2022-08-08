be853e10b45bd1b2b46bffcac1146e2583f82d2a
Marco <marco.salmi89@gmail.com>
2022-08-08 13:35:41 +0200

    Fix

--- include/staff/csc.inc.php
+++ include/staff/csc.inc.php
@@ -173,7 +173,6 @@ document.getElementById('cscino').value='Submitting, please wait...';">
         $keys[]='subject';
         $keys[]='message';
         $keys[]='ip';
-        $keys[]='ref_num';
 
         $key_name = array_search('customer_middle_name', $keys);
         $key_topicId = array_search('group_last_name', $keys);
@@ -718,7 +717,7 @@ document.getElementById('cscino').value='Submitting, please wait...';">
             $line[]=isset($line[$key_subject])?$line[$key_subject]:null;
             $line[]=isset($line[$key_message])?$line[$key_message]:null;
             $line[]=$_SERVER['REMOTE_ADDR'];
-            $line[] = isset($line[$key_message])?$line[$key_message]:null;
+            //$line[] = isset($line[$key_message])?$line[$key_message]:null;
 
             array_map('db_input', $line);
 
@@ -734,8 +733,6 @@ document.getElementById('cscino').value='Submitting, please wait...';">
 
             $csv[] = array_combine($keys, $line);
 
-            print_r($csv);
-            exit();
         }
 
 
