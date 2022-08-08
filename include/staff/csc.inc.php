a78528ac25c14a1c83c000cfec4e8370ec48c93d
Marco <marco.salmi89@gmail.com>
2022-08-08 12:59:04 +0200

    Fix

--- include/staff/csc.inc.php
+++ include/staff/csc.inc.php
@@ -730,8 +730,8 @@ document.getElementById('cscino').value='Submitting, please wait...';">
 
             $csv[] = array_combine($keys, $line);
 
-            //print_r($csv);
-
+            print_r($csv);
+            exit();
         }
 
 
