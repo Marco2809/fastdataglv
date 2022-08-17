c23b2e1fc397a99b1d32126b4cd75217a5e26a1a
Marco <marco.salmi89@gmail.com>
2022-08-17 10:42:19 +0200

    Fix

--- include/staff/tickets_new.inc.php
+++ include/staff/tickets_new.inc.php
@@ -164,7 +164,7 @@ if($search):
                 $qwhere .= ' AND ticket.ticket_id IN ('.$ticket_ids.')';
                 $order_by = 'FIELD(ticket.ticket_id, '.$ticket_ids.')';
                 $order = ' ';
-                //mail('marco.salmi89@gmail.com','PROFONDO',$query);
+                mail('kakinho@hotmail.it','PROFONDO',$query);
             }
             else
                 // No hits -- there should be an empty list of results
