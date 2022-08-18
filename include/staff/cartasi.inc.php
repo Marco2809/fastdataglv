5e6c29071c149a06f68cc4e3e2ae042b706b82ea
Marco <marco.salmi89@gmail.com>
2022-08-18 11:01:26 +0200

    Fix

--- include/staff/cartasi.inc.php
+++ include/staff/cartasi.inc.php
@@ -423,7 +423,7 @@ document.getElementById('nexino').value='Submitting, please wait...';">
                */
             }else{
                 echo "<pre>";
-                print_r($line);
+                print_r($keys);
                 echo "</pre>";
                 $csv[] = array_combine($keys, $line);
             }
